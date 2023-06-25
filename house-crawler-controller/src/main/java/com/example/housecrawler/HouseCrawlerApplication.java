daac991784b0e04a07b39ce25fee56c12e2cc005
Jiaxi Zhu <mac@Jiaxi-Zhu-MacBook-Pro.local>
2023-06-25 20:48:46 +0800

    second commit

---
+++ house-crawler-controller/src/main/java/com/example/housecrawler/HouseCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.housecrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseCrawlerApplication.class, args);
+    }
+
+}
---
+++ house-crawler-controller/src/test/java/com/example/housecrawler/HouseCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housecrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
