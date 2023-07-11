b795858ba67869659ba225649be1f23532711687
Haosong Liu <haosong.liu@wirelesscar.com>
2023-07-11 11:00:38 +0800

    test access

--- house-crawler-controller/src/test/java/com/example/housecrawler/HouseCrawlerApplicationTests.java
+++ house-crawler-controller/src/test/java/com/example/housecrawler/HouseCrawlerApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.housecrawler;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HouseCrawlerApplicationTests {
 
+    @BeforeAll
+    void setup(){}
     @Test
     void contextLoads() {
     }
